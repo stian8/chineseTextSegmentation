224940a103e35fe1d0ad804c6f5b0e1f0ab5d59d
Katherine Wang <kwang@Ks-MacBook-Pro.local>
2018-12-03 18:39:08 -0500

    Generalizing functions and adjusting data format

--- chinese_helper.py
+++ chinese_helper.py
@@ -4,20 +4,20 @@ import sys
 import jieba.posseg as pseg
 import csv
 
-INPUT = "truth_result.txt"
-INTERMEDIATE = "truth_clean.txt"
-OUTPUT  = "training.csv"
+#INPUT = "truth_result.txt"
+#INTERMEDIATE = "truth_clean.txt"
+#OUTPUT  = "training.csv"
 PUNC = set(['，',',','.','!',':',';','“', '"', '，', '。',
             '、', '！', '；', '？','……', '?', '：', '”'])
 CON = set(['的','地', '得']) # what to do about le, bu
-def main():
-    clean_text()
-    create_csv()
+def main(inp, imed, out):
+    clean_text(inp, imed)
+    create_csv(imed, out)
 
-def clean_text():
-    write_file = open(INTERMEDIATE, mode="w", encoding='utf-8-sig')
+def clean_text(inp, imed):
+    write_file = open(imed, mode="w", encoding='utf-8-sig')
     result = ""
-    with open(INPUT, mode='rt', encoding="utf8") as f:
+    with open(inp, mode='rt', encoding="utf8") as f:
         data=f.read().replace('\n', '')
         data = re.sub(' +', '', data)
         data = re.sub('[”“‘’]', '', data)
@@ -46,14 +46,14 @@ def clean_text():
         write_file.write(''.join(final))
     write_file.close()
 
-def create_csv():
-    write_file = open(OUTPUT, mode="w", encoding='utf-8-sig')
+def create_csv(imed, out):
+    write_file = open(out, mode="w", encoding='utf-8-sig')
     w = csv.writer(write_file)
     window = [None for _ in range(5)]
     # No being which number split it is
     w.writerow(["Label", "No", "B2", "B1", "F1", "F2", "POSB2",
                "POSB1", "POSF1", "POSF2"])
-    with open(INTERMEDIATE, mode='rt', encoding="utf8") as f:
+    with open(imed, mode='rt', encoding="utf8") as f:
         data=f.read().replace('\n', '')
         sentences = re.findall('.*?[！。？]', data)
         for s in sentences:
@@ -88,17 +88,7 @@ def create_csv():
                                 POSF1, POSF2])
     write_file.close()
 if __name__ == "__main__":
-    main()
-
-"""for line in f:
-            words = pseg.cut(line)
-            # unigram and bigram adjustments can be made here
-            previous = None
-            for word, flag in words:
-                if word not in PUNC:
-                    if word in CON and previous:
-                        #print(word + previous)
-                        w.writerow([previous + word, 'ADJ'])
-                    else:
-                        w.writerow([word, flag])
-                    previous = word"""
+    inp = sys.argv[1]
+    imed = sys.argv[2]
+    out = sys.argv[3]
+    main(inp, imed, out)
--- parse.py
+++ parse.py
@@ -2,16 +2,17 @@ import re
 import sys
 import os
 
-DIR = 'raw_data/'
-def main():
-    write_all_text(DIR)           
+#DIR = 'raw_data/'
+#OUT = 'processed_data/'
+def main(input_dir, output_dir):
+    write_all_text(input_dir, output_dir)           
 
-def write_all_text(text_dir):
+def write_all_text(text_dir, output_dir):
     """Function to write all the groupings, each on its own line,
     to a text file per subject."""
     subject = None
     for subdir, dirs, files in os.walk(text_dir):
-        all_txt = []
+        all_txt = ''
         for file in files:
             if file != '.DS_Store': # DS store
                 path = os.path.join(subdir, file)
@@ -19,21 +20,22 @@ def write_all_text(text_dir):
                     inp = f.readlines()
                     for line in inp:
                         # replacing delineation characters
-                        line = re.sub('[“”、?？！!:/,.;；：／，。\(\)]',' ', line)
-                        line = re.sub(' +',' ', line)
-                        all_txt.extend(line.split())
+                        #line = re.sub('[“”、?？！!:/,.;；：／，。\(\)]',' ', line)
+                        line = re.sub(' +','', line)
+                        all_txt += line
         index = subdir.rfind('/')
         subject = subdir[index+1:]
         if subject:
             # I was thinking that here we could write to csv then load into pandas
-            write_path = 'processed_data/' + subject + '_result.txt'
+            write_path = output_dir + subject + '_result.txt'
             write_file = open(write_path,"w+")
-            for g in all_txt:
-                write_file.write(g + "\n")
+            write_file.write(all_txt)
+            #for g in all_txt:
+                #write_file.write(g + "\n")
             write_file.close()
     return ''.join(all_txt) # not doing anything currently
 
 if __name__ == "__main__":
-    #data = sys.argv[1]
-    #main(data)
-    main()
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    main(input_dir, output_dir)
